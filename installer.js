ce061d2fdaadf3bd78b4c755e1a995ec123c4630
vlo <123765798+vlothedev@users.noreply.github.com>
2023-12-16 21:06:22 +0000

    Update installer.js

--- installer.js
+++ installer.js
@@ -1,52 +1,45 @@
 const manifest = {
-  "manifest_version": 2,
-  "name": "your extension",
-  "permissions": ["accessibilityFeatures.modify", "storage", "accessibilityFeatures.read", "accessibilityPrivate", "activeTab", "activityLogPrivate", "autofillPrivate", "autotestPrivate", "background", "bookmarks", "brailleDisplayPrivate", "browsingData", "certificateProvider", "chromePrivate", "chromeosInfoPrivate", "clipboardRead", "clipboardWrite", "commandLinePrivate", "commands.accessibility", "contentSettings", "contextMenus", "cookies", "crashReportPrivate", "cryptotokenPrivate", "debugger", "declarativeContent", "desktopCapture", "desktopCapturePrivate", "developerPrivate", "documentScan", "downloads", "downloads.open", "downloads.shelf", "downloads.ui", "echoPrivate", "enterprise.deviceAttributes", "enterprise.hardwarePlatform", "enterprise.networkingAttributes", "enterprise.platformKeys", "enterprise.platformKeysPrivate", "enterprise.reportingPrivate", "experimental", "favicon", "fileBrowserHandler", "fontSettings", "gcm", "geolocation", "history", "identity", "identity.email", "identityPrivate", "idltest", "imageWriterPrivate", "input", "inputMethodPrivate", "languageSettingsPrivate", "lockWindowFullscreenPrivate", "login", "loginScreenStorage", "loginScreenUi", "loginState", "management", "mdns", "mediaPlayerPrivate", "mediaRouterPrivate", "notifications", "pageCapture", "passwordsPrivate", "platformKeys", "plugin", "printing", "printingMetrics", "privacy", "processes", "proxy", "resourcesPrivate", "rtcPrivate", "safeBrowsingPrivate", "scripting", "search", "sessions", "settingsPrivate", "sharedStoragePrivate", "sidePanel", "speechRecognitionPrivate", "systemPrivate", "tabCapture", "tabGroups", "tabs", "terminalPrivate", "topSites", "transientBackground", "tts", "ttsEngine", "usersPrivate", "wallpaper", "webAuthenticationProxy", "webNavigation", "webcamPrivate", "webrtcAudioPrivate", "webrtcDesktopCapturePrivate", "webrtcLoggingPrivate", "webrtcLoggingPrivate.audioDebug", "webstorePrivate", "wmDesksPrivate", "offscreen", "webRequest", "userScripts", "<all_urls>"],
-  "version": "0.0.0"
-}
-
-const HELP_URL = "https://github.com/bypassiwastaken/skebstore";
-
-let [
-    help
-] = document.querySelectorAll('svg')
-
-help.addEventListener('click', () => {
-    chrome.tabs.create({ url: HELP_URL })
-})
+  "manifest_version": 3,
+  "name": "Your Extension",
+  "permissions": ["storage", "activeTab", "downloads", "webNavigation"],
+  "version": "1.0.0"
+};
 
 function installId(id) {
-  return new Promise((resolve, reject) => {
-    chrome.webstorePrivate.beginInstallWithManifest3(
-      {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await chrome.webstorePrivate.beginInstallWithManifest3({
         id: id,
         manifest: JSON.stringify(manifest)
-      },
-      () => {
-        if (chrome.runtime.lastError) reject(chrome.runtime.lastError.message)
-        chrome.webstorePrivate.completeInstall(id, resolve);
+      });
+      await chrome.webstorePrivate.completeInstall(id, resolve);
+    } catch (error) {
+      if (error.message.includes("Invalid extension ID")) {
+        reject("Please enter a valid extension ID.");
+      } else {
+        reject("An error occurred during installation.");
       }
-    );
-  })
+    }
+  });
 }
 
-let button = document.querySelector('button')
-let input = document.querySelector('input')
-let error = document.querySelector('.error')
-let version = document.querySelector('.version')
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('button');
+  const input = document.querySelector('input');
+  const error = document.querySelector('.error');
 
-version.textContent = "v" + chrome.runtime.getManifest().version
+  button.addEventListener('click', async () => {
+    try {
+      await installId(input.value);
+      error.textContent = '';
+    } catch (err) {
+      error.textContent = err;
+    }
+  });
 
-button.addEventListener('click', () => {
-  installId(input.value).then(() => {
-    error.textContent = ""
-  }).catch((err) => {
-    error.textContent = err
-  })
-})
-
-input.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    button.click()
-  }
-})
+  input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      button.click();
+    }
+  });
+});
